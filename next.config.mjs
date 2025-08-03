0a13e489ea683bb7adc09446a2e09a2571547cab
VishalPachpor <Vishalpatil080502@gmail.com>
2025-08-03 08:08:53 +0530

    Whitelist added

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "https://docs.stratifi.xyz/docs/intro",
+        permanent: true, // 301 redirect
+      },
+      {
+        source: "/whitelist",
+        destination: "https://app.youform.com/forms/bbbn2d9r",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
